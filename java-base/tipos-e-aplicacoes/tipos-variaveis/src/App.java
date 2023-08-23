c2f79e0576fb684596330052ecdb3f7867670bca
Igor Fernandes dos Santos <igorfercontato@gmail.com>
2023-08-23 13:58:51 -0300

    operadores

---
+++ java-base/tipos-e-aplicacoes/tipos-variaveis/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte idade = 100;
+        short porta = 3000;
+        long velocidadeDaLuz = 299_792_458L;
+        float pi = 3.14f;
+        double salarioMinimo = 2.500;
+
+        System.out.println(salarioMinimo + " " + pi);
+        System.out.println(velocidadeDaLuz);
+    }
+}
