f9d223dd8585562ed8c1f29a188e803f0a5a0947
Igor Fernandes dos Santos <igorfercontato@gmail.com>
2023-08-28 17:13:18 -0300

    Desafio de terminal feito e adicionado

--- java-base/desafios/ContaBanco/src/ContaTerminal.java
+++ java-base/desafios/ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
-    private int numero;
-    private String agencia;
-    private String nomeCliente;
-    private double saldo;
-    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do cliente!");
+        String nomeCliente = scanner.next();
+
+        Conta conta = new Conta(numero, agencia, nomeCliente);
+
+        String mensagem = String.format(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque",
+                conta.getNomeCliente(),
+                conta.getAgencia(),
+                conta.getNumero(),
+                conta.getSaldo());
+
+        System.out.println(mensagem);
     }
 }
