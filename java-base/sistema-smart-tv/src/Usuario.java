01c6087c37f89c9ced5078407326c617632389c8
Igor Fernandes dos Santos <igorfercontato@gmail.com>
2023-08-28 14:44:22 -0300

    metodos

---
+++ java-base/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        
+
+        System.out.println("Ligada: " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+    }
+}
