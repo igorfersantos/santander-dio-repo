f285c573b8f0bb5a2086b6f1fe43e2fda6dc7c2c
Igor Fernandes dos Santos <igorfercontato@gmail.com>
2023-08-29 14:19:11 -0300

    Subseções de Exceções e Repetições adicionados a seção de controle de fluxo

---
+++ java-base/aulas/controle-de-fluxo/repeticao/for-break-while-etc/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = { "Igor", "João", "Maria", "Julia" };
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Aluno " + i + " é " + alunos[i]);
+        }
+    }
+}
